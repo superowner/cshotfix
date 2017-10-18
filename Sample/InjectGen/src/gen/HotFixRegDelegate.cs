c180a345a0c32d372ce942f4b02a39da8b8c1da5
lichunlincn <lichunlincn@users.noreply.github.com>
2017-10-18 19:01:20 +0800

    新增一个Ilruntime需要用到的委托注册类，用于注册这些委托

---
+++ Sample/InjectGen/src/gen/HotFixRegDelegate.cs
@@ -0,0 +1,17 @@
+//======================================================================
+//                                                                       
+//        created by lichunlin                                           
+//        qq:576067421                                                   
+//        git:https://github.com/lichunlincn/cshotfix                    
+//                                                                       
+//====================================================================== 
+using System.Collections; 
+using System; 
+namespace HotFix
+{
+       public class HotFixRegDelegate
+       {
+           public delegate void delegate_0(object _this);
+           public delegate Int32 delegate_2(object _this,Single _param0_Single, ref UnityEngine.Vector3 _param1_Vector3,String _param2_String, ref System.Int32 _param3_Int32, ref object _param4_ClassData, out System.String _param5_String);
+       }
+}
