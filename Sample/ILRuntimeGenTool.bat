32944d12f3d34b22bf29348a4094ae9c96946c1a
lichunlincn <lichunlincn@users.noreply.github.com>
2018-01-25 16:29:52 +0800

    删除之前的

--- Sample/ILRuntimeGenTool.bat
+++
@@ -1,9 +0,0 @@
-echo //        created by lichunlin
-echo //        qq:576067421
-echo //        git:https://github.com/lichunlincn/cshotfix
-
-
-rd /s /Q "%cd%\ILRuntimeGen\src"
-md "%cd%\ILRuntimeGen\src"
-ILRuntimeGenTool\bin\Debug\ILRuntimeGenTool.exe "%cd%\HotFixDll\bin\Debug\HotFixDll.dll.bytes" "%cd%\ILRuntimeGen\src"
-pause
